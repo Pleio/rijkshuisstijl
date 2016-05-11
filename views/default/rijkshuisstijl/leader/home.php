b2d21d2e85e9107972fa99d5cea858d3c0c0f5d9
Bart Jeukendrup <bart@jeukendrup.nl>
2016-05-11 13:35:48 +0200

    updated: change title to site header

--- views/default/rijkshuisstijl/leader/home.php
+++ views/default/rijkshuisstijl/leader/home.php
@@ -1,7 +1,11 @@
+<?php
+$site = elgg_get_site_entity();
+?>
+
 <div class="rhs-lead rhs-lead--home">
   <div style="background-image:url('<?php echo rijkshuisstijl_asset("image/lead--home.jpg"); ?>');" class="rhs-lead__background"></div>
   <div class="rhs-container">
-    <h1 class="rhs-home__title">Forum Fiscaal Dienstverleners</h1>
+    <h1 class="rhs-home__title"><?php echo $site->name; ?></h1>
     <form action="/search" data-autocomplete="" class="rhs-lead__search">
       <label for="lead-search" class="rhs-lead__search__label"><span class="rhs-icon-circle-arrow"></span><span class="rhs-icon-search"></span></label>
       <button type="submit" class="rhs-autocomplete__submit-button"><span class="rhs-icon-search"></span></button>
