75af4c44f08ebcb4b4a8e1f5151e03b75f3baed8
Bart Jeukendrup <bart@jeukendrup.nl>
2016-10-17 18:28:45 +0200

    improve styling of most valuable user

--- views/default/rijkshuisstijl/elements/user_row.php
+++ views/default/rijkshuisstijl/elements/user_row.php
@@ -7,12 +7,12 @@ $user = elgg_extract("user", $vars);
     <table class="rhs-card-list__table">
         <tbody>
             <tr>
-                <td class="rhs-card-list__date">
+                <td class="rhs-card-list__user" style="">
                     <a href="<?php echo $user->getURL(); ?>" title="<?php echo $user->name; ?>" class="rhs-card-user-content__profile">
                       <img src="<?php echo $user->getIconURL('small') ?>">
                     </a>
                 </td>
-                <td class="rhs-card-list__text" style="min-width: 20rem;">
+                <td class="rhs-card-list__text rhs-card-list__text--user">
                     <?php echo $user->name; ?>
                 </td>
             </tr>
