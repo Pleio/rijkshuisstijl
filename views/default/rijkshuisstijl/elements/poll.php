1235febf79665e5e8594b63ced5ec325810e42ad
bart-j <bart@jeukendrup.nl>
2017-08-07 10:40:32 +0200

    do not show number of votes when not voted on the poll

--- views/default/rijkshuisstijl/elements/poll.php
+++ views/default/rijkshuisstijl/elements/poll.php
@@ -2,12 +2,13 @@
 elgg_load_library("elgg:polls");
 
 $poll = rijkshuisstijl_get_latest_poll();
+$can_vote = rijkshuisstijl_can_vote_on_poll($poll);
 ?>
 
 <?php if ($poll): ?>
     <a id="poll"></a>
     <div class="rhs-card-poll-container">
-    <?php if (rijkshuisstijl_can_vote_on_poll($poll)): ?>
+    <?php if ($can_vote): ?>
         <div class="rhs-card-poll rhs-card-poll--margin-bottom" data-poll>
             <div class="rhs-card-poll__icon"><img src="/mod/rijkshuisstijl/assets/image/poll.svg"></div>
             <?php echo elgg_view_form("rijkshuisstijl/poll", ["class" => "rhs-card-poll__content", "action" => "action/polls/vote", "method" => "POST"], ["poll" => $poll]); ?>
