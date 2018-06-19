dad7c0b7c744df3c30a001c16bae2489f188ca2b
Bart <bart@jeukendrup.nl>
2018-06-20 00:31:04 +0200

    Directly included svg in overview

--- views/default/object/news.php
+++ views/default/object/news.php
@@ -119,23 +119,25 @@ if ($full) {
         if (!is_array($tags)) {
             $tags = [$tags];
         }
+        ?>
 
-        if (in_array('juris', $tags)) {
-            echo '<span class="rhs-icon-jurisprudentie rhs-news-list__item__category__icon"></span>';
-        }
+        <?php if (in_array('juris', $tags)): ?>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 16.7" width="20px" height="16.7px"><path fill="#b4b4b4" d="M0.7,14.9h0.7V14h10v0.9h0.7c0.4,0,0.7,0.4,0.7,0.9c0,0.5-0.3,0.9-0.7,0.9H0.7c-0.4,0-0.7-0.4-0.7-0.9 C0,15.3,0.3,14.9,0.7,14.9"/><path fill="#b4b4b4" d="M8.3,11.6C8,11.9,7.5,12,7.2,11.7L3.7,8.2C3.4,8,3.5,7.5,3.8,7.1C4.2,6.8,4.7,6.7,4.9,7l3.5,3.5 C8.7,10.8,8.6,11.3,8.3,11.6"/><path fill="#b4b4b4" d="M15.1,4.8c-0.3,0.3-0.8,0.4-1.1,0.1l-3.5-3.5c-0.3-0.3-0.2-0.8,0.1-1.1C11,0,11.5-0.1,11.8,0.2l3.5,3.5 C15.5,3.9,15.5,4.4,15.1,4.8"/><path fill="#b4b4b4" d="M10,1.9l3.5,3.5L11.9,7l7.8,7.8c0.2,0.2,0.2,0.4,0.2,0.6c0,0.1,0,0.2-0.1,0.3c-0.1,0.2-0.3,0.4-0.5,0.5c-0.1,0-0.2,0.1-0.3,0.1c-0.2,0-0.4-0.1-0.6-0.2l-7.8-7.8l0,0L8.9,10L5.4,6.5L10,1.9z"/></svg>
+        <?php endif; ?>
 
-        if (in_array('blog', $tags)) {
-            echo '<span class="rhs-icon-pencil rhs-news-list__item__category__icon"></span>';
-        }
+        <?php if (in_array('blog', $tags)): ?>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 172.9 172.9" width="20px" height="16.7px"><path fill="#b4b4b4" d="M170.1,38.8c3.7-3.7,3.7-9.8,0-13.5L147.6,2.8c-3.7-3.7-9.8-3.7-13.5,0l-17.6,17.6l36,36L170.1,38.8z M0,136.9v36h36L142.2,66.7l-36-36L0,136.9z"/></svg>
+        <?php endif; ?>
 
-        if (in_array('handreiking', $tags)) {
-            echo '<span class="rhs-icon-file rhs-news-list__item__category__icon"></span>';
-        }
+        <?php if (in_array('handreiking', $tags)): ?>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="20px" height="16.7px"><path fill="#b4b4b4" d="M288 248v28c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-28c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12zm-12 72H108c-6.6 0-12 5.4-12 12v28c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-28c0-6.6-5.4-12-12-12zm108-188.1V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V48C0 21.5 21.5 0 48 0h204.1C264.8 0 277 5.1 286 14.1L369.9 98c9 8.9 14.1 21.2 14.1 33.9zm-128-80V128h76.1L256 51.9zM336 464V176H232c-13.3 0-24-10.7-24-24V48H48v416h288z"/></svg>
+        <?php endif; ?>
 
-        if (in_array('achtergrond', $tags)) {
-            echo '<span class="rhs-icon-newspaper rhs-news-list__item__category__icon"></span>';
-        }
+        <?php if (in_array('achtergrond', $tags)): ?>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" width="20px" height="16.7px"><path fill="#b4b4b4" d="M552 64H112c-20.858 0-38.643 13.377-45.248 32H24c-13.255 0-24 10.745-24 24v272c0 30.928 25.072 56 56 56h496c13.255 0 24-10.745 24-24V88c0-13.255-10.745-24-24-24zM48 392V144h16v248c0 4.411-3.589 8-8 8s-8-3.589-8-8zm480 8H111.422c.374-2.614.578-5.283.578-8V112h416v288zM172 280h136c6.627 0 12-5.373 12-12v-96c0-6.627-5.373-12-12-12H172c-6.627 0-12 5.373-12 12v96c0 6.627 5.373 12 12 12zm28-80h80v40h-80v-40zm-40 140v-24c0-6.627 5.373-12 12-12h136c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H172c-6.627 0-12-5.373-12-12zm192 0v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0-144v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0 72v-24c0-6.627 5.373-12 12-12h104c6.627 0 12 5.373 12 12v24c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12z"/></svg>
+        <?php endif; ?>
 
+        <?php
     }
 
     if ($container instanceof ElggGroup) {
